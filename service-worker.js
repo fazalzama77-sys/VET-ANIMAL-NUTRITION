7a4f911d8fecbcd4e1395bb284415dd53f1338ac
fazalzama77-sys <fazalzama77@gmail.com>
2026-09-13 19:41:10 +0530

    Update Animal Nutrition Studio content (13-09-2026 19:41:10.11)

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
    Animal Nutrition Studio (B.V.Sc & A.H.)
    ============================================================ */
 
-var CACHE_VERSION = "vanut-v6";
+var CACHE_VERSION = "vanut-v7";
 var SHELL_CACHE = CACHE_VERSION + "-shell";
 var IMG_CACHE = CACHE_VERSION + "-img";
 
